ad86ed8e965f47cca6f66a520390408ba1db1802
Ainul7030 <109273401+Ainul7030@users.noreply.github.com>
2022-11-21 14:05:28 +0530

    Add files via upload

---
+++ pattern6.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+int main ()
+
+{
+
+int n=5,m=5;
+
+
+for (int i=0;i<=n;i++){
+
+    for (int k=0;k<=m;k++){
+    for (int j=0;j<=n;j++)
+    
+
+    if (i+j==n/2 || i-j==-n/2)
+
+    printf("7");
+    else
+    printf(" ");
+    }
+
+    printf("\n");
+
+   
+}
+
+
+
+
+
+
+
+
+return 0;
+
+}
